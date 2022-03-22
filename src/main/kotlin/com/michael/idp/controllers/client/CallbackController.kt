4d07a47a3ceaf143c24c3080dc0ddfcaa8af0c5c
michael dang <michael.dang@workday.com>
2022-03-21 17:20:32 -0700

    add client to work with okta idp

--- src/main/kotlin/com/michael/idp/controllers/client/CallbackController.kt
+++ src/main/kotlin/com/michael/idp/controllers/client/CallbackController.kt
@@ -7,11 +7,15 @@ import com.michael.idp.models.TokenInput
 import com.michael.idp.models.TokenOutput
 import com.michael.idp.utils.Constants
 import com.michael.idp.utils.Json
+import okhttp3.FormBody
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -30,9 +34,91 @@ import org.springframework.web.servlet.ModelAndView
 @RequestMapping("/client")
 class CallbackController(private val service: ClientConfigService) {
 
+    val logger = LoggerFactory.getLogger(CallbackController::class.java)
+
+    @GetMapping("/okta-callback")
+    fun oktaToken(
+        @RequestParam code: String,
+        @RequestParam state: String
+    ): ModelAndView {
+        logger.info("hey!!!!!!!!")
+        println("callback $code")
+
+
+        val clientId = "0oa4a6dhyl82JZLWs5d7"
+        val clientSecret = "ZidUekITb7xTSPWL-0daj_1Q-xU8FOX_jX7lrgla"
+        //https://dev-22415998.okta.com/oauth2/default/.well-known/oauth-authorization-server
+        //https://dev-22415998.okta.com/oauth2/default/v1/authorize
+
+//        if (LocalCache.state != state) {
+//            println("BAD STATE")
+//        }
+
+        val localConfig = service.getLocalClientConfig()
+
+//        val tokenInput = TokenInput(
+//            code,
+//            "http://localhost:8080/client/okta-callback",
+//            client_id = clientId,
+//            clientSecret,
+//            GRANT_TYPE_AUTHORIZATION_CODE
+//        )
+        val client = OkHttpClient()
+
+
+//        val postBody: RequestBody = Json.toJson(tokenInput).toRequestBody(MediaType.APPLICATION_JSON_VALUE.toMediaTypeOrNull())
+
+//        val postBody: RequestBody =
+//            Json.toJson(tokenInput).toRequestBody(MediaType.APPLICATION_FORM_URLENCODED_VALUE.toMediaType())
+
+        val postBody: RequestBody = FormBody.Builder()
+            .add("code", code)
+            .add("client_id", clientId)
+            .add("client_secret", clientSecret)
+            .add("redirect_uri", "http://localhost:8080/client/okta-callback")
+            .add("grant_type", GRANT_TYPE_AUTHORIZATION_CODE)
+            .build()
+//        val buffer = Buffer()
+
+
+        //TODO - Headers
+        val oktaAuthServer = "https://dev-22415998.okta.com/oauth2/default/v1/token"
+        val request: Request = Request.Builder()
+            .url(oktaAuthServer)
+            //Not sure why this doenst wokr hoenstly
+            .post(postBody)
+            .build()
+
+
+//        request.body!!.writeTo(buffer)
+//        logger.info(buffer.readUtf8())
+//        buffer.close()
+
+
+        client.newCall(request).execute().use { response ->
+            if (response.isSuccessful) {
+                val tokenOutput: TokenOutput? = Json.fromJson<TokenOutput>(response.body!!.string())
+
+
+                val mv = ModelAndView("client/login")
+                mv.addObject("accessToken", tokenOutput!!.access_token)
+
+                return mv
+            } else {
+                logger.error(response.body!!.string())
+                println(response.code)
+                println(response.body!!.string())
+                throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "bad stuff")
+            }
+
+        }
+    }
+
     @GetMapping("/callback")
-    fun token(@RequestParam code: String,
-              @RequestParam state: String): ModelAndView {
+    fun token(
+        @RequestParam code: String,
+        @RequestParam state: String
+    ): ModelAndView {
         println("callback $code")
 
 
@@ -42,15 +128,16 @@ class CallbackController(private val service: ClientConfigService) {
 
         val localConfig = service.getLocalClientConfig()
 
-        val tokenInput = TokenInput(code, "","","", GRANT_TYPE_AUTHORIZATION_CODE)
+        val tokenInput = TokenInput(code, "", "", "", GRANT_TYPE_AUTHORIZATION_CODE)
         val client = OkHttpClient()
-        val postBody: RequestBody = Json.toJson(tokenInput).toRequestBody(MediaType.APPLICATION_JSON_VALUE.toMediaTypeOrNull())
+        val postBody: RequestBody =
+            Json.toJson(tokenInput).toRequestBody(MediaType.APPLICATION_JSON_VALUE.toMediaTypeOrNull())
 
 
         //TODO - Headers
         val request: Request = Request.Builder()
             .url(LOCALHOST + "/server/token")
-                //Not sure why this doenst wokr hoenstly
+            //Not sure why this doenst wokr hoenstly
             .header(Constants.CSRF_TOKEN_HEADER, "newToken")
             .post(postBody)
             .build()
@@ -58,16 +145,16 @@ class CallbackController(private val service: ClientConfigService) {
 
         client.newCall(request).execute().use { response ->
             if (response.isSuccessful) {
+                logger.info("success")
                 val tokenOutput: TokenOutput? = Json.fromJson<TokenOutput>(response.body!!.string())
 
 
-
                 val mv = ModelAndView("client/login")
-                mv.addObject("accessToken", tokenOutput!!.accessToken)
+                mv.addObject("accessToken", tokenOutput!!.access_token)
 
                 return mv
             } else {
-                println(response.code)
+                logger.error("failure exchanging token")
                 println(response.body!!.string())
                 throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "bad stuff")
             }
--- src/main/kotlin/com/michael/idp/models/TokenInput.kt
+++ src/main/kotlin/com/michael/idp/models/TokenInput.kt
@@ -5,19 +5,20 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class TokenInput(
     val code: String?,
-    val redirect_uri: String?,
-    val client_id: String?,
-    val client_secret: String?,
-    val grant_type: String //Required dog
+    val redirect_uri: String,
+    val client_id: String,
+    val client_secret: String,
+    val grant_type: String
 )
 
 //https://datatracker.ietf.org/doc/html/rfc6749#section-5.1
 data class TokenOutput(
-    val tokenType: String, //https://datatracker.ietf.org/doc/html/rfc6749#section-7.1 ("Bearer")
-    val idToken: String,
-    val accessToken: String,
-    val expiresIn: Int, // RECOMMENDED in spec
-    val refreshToken: String? = null
+    val token_type: String, //https://datatracker.ietf.org/doc/html/rfc6749#section-7.1 ("Bearer")
+    val id_token: String,
+    val access_token: String,
+    val expires_in: Int, // RECOMMENDED in spec
+    val refresh_token: String? = null,
+    val scope: String? = null
 )
 
 const val GRANT_TYPE_AUTHORIZATION_CODE = "authorization_code"
